1c1cb82dc7a53dfb0517a77cd7f999acaae47809
achi <achievendar@gmail.com>
2019-09-25 00:04:46 +0300

    -UPPERCASE constants -sync readme with comments -improved amount and date logic

--- reconciliationEngine/index.js
+++ reconciliationEngine/index.js
@@ -1,41 +1,37 @@
-const editingDistance = 1;
-const datesDistance = 1000 * 60 *60 * 24 * 7;
-const amountDistance = 100;
+const EDITING_DISTANCE_THRESHOLD = 1;
+const DATE_DURATION_THRESHOLD = 1000 * 60 *60 * 24;
+const AMOUNT_DIFFERENCE_THRESHOLD = 100;
 
 exports.build = (payables) => {
     this.payables = payables;
-}
+};
 
 //if I had more time I would support multiple reference ids, as in the example
-//There is probably some duplicate login  for matching 2 out of 3, if I had more time I would investigate and reduce the running time of it
-//If I had more time I would write thorough tests with mocha.js for this
+//There is some duplicate logic in the matching function (as I implemented this), specifically in the matching of 2 out of 3. It is calling twice for each function. if I had more time I would investigate and reduce this duplicacy, hence - reduce the running time of it, as well as ease of readability
+//As well I would have implement some partial score. For example exact matching give spartial score of 1, editing distance of 1 would give a partial score of 50 and editing distance of 2 would give 25 etc.. (doesn't have to to be scaled by 2, but a Geometric progression of some sort, give or take fixed variables. So it would be in the pattern of f(x) = a + (1/factor)*distance, obviously with different factor for date/amount/referenceId
+//If I had more time I would write thorough tests with mocha.js for this function
 //I also started to implement editing distance between the dates (for example same date, give or take 1 day a month ago, or a year ago, but I don't have time for this right now)
-//I'm assuming correctness of input from client (if not handled by web server) and from json file
+//I'm assuming correctness of input from client (implemented by web server) and from json file/graphQL
 
 exports.getPayables = (payment) => {
     const identicalByReference = this.payables.filter((payable) => {
         return payment.payment_reference === payable.referenceId;
     });
-    const identicalByDate = this.payables.filter((payable) => {
-        return payment.payment_date === payable.dateOccurred;
-    });
-    const identicalByAmount = this.payables.filter((payable) => {
-        return payment.amount === payable.amount;
+    const identicalByDateAndAmount = this.payables.filter((payable) => {
+        return payment.payment_date === payable.dateOccurred && payment.amount === payable.amount;
     });
     const similarByReferenceAndDate = this.payables.filter((payable) => {
-        const dateDistance = getEditDistance(payment.payment_reference, payable.referenceId);
-        return getEditDistance(payment.payment_reference, payable.referenceId) <= editingDistance && dateDistance <= datesDistance;
+        return getEditDistance(payment.payment_reference, payable.referenceId) <= EDITING_DISTANCE_THRESHOLD && getDateDistance(payment.payment_date, payable.dateOccurred) <= DATE_DURATION_THRESHOLD;
     });
     const similarByReferenceAndAmount = this.payables.filter((payable) => {
-        return getEditDistance(payment.payment_reference, payable.referenceId) <= editingDistance && getAmountDistance(payment.amount, payable.amount) <= amountDistance;
+        return getEditDistance(payment.payment_reference, payable.referenceId) <= EDITING_DISTANCE_THRESHOLD && getAmountDistance(payment.amount, payable.amount) <= AMOUNT_DIFFERENCE_THRESHOLD;
     });
     const similarByDateAndAmount = this.payables.filter((payable) => {
-        return getDateDistance(payment.payment_date, payable.dateOccurred) <= datesDistance && getAmountDistance(payment.amount, payable.amount) <= amountDistance;
+        return getDateDistance(payment.payment_date, payable.dateOccurred) <= DATE_DURATION_THRESHOLD && getAmountDistance(payment.amount, payable.amount) <= AMOUNT_DIFFERENCE_THRESHOLD;
     });
     const uniqueMatches = [].concat(
         identicalByReference,
-        identicalByDate,
-        identicalByAmount,
+        identicalByDateAndAmount,
         similarByReferenceAndDate,
         similarByReferenceAndAmount,
         similarByDateAndAmount).filter((payment, index, self) => {
@@ -57,6 +53,8 @@ function getDateDistance (d1,d2) {
     return diffMS;
 }
 
+
+//code from https://gist.github.com/andrei-m/982927
 function getEditDistance (str1, str2){
     if(str1.length == 0) return str2.length;
     if(str2.length == 0) return str1.length;
